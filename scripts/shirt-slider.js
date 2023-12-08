759d24b348a5f0e75637ee4e48b38f2e2044c909
daATva <riven1231@mail.ru>
2023-12-09 01:22:00 +0400

    review slider

--- scripts/shirt-slider.js
+++ scripts/shirt-slider.js
@@ -1,35 +1,28 @@
+
 (function () {
   const slides = document.querySelectorAll(".fade-slider__shirt");
-
   const activeClass = "fade-slider__shirt--visible";
-
   const lessons = document.querySelector(".circle");
 
-
-  function clearList(){
-    slides[1].classList.remove(activeClass);
-    slides[2].classList.remove(activeClass);
-    slides[3].classList.remove(activeClass);
-    slides[4].classList.remove(activeClass);
-    slides[0].classList.remove(activeClass);
+  function clearList() {
+    slides.forEach(slide => slide.classList.remove(activeClass));
   }
 
-
   lessons.addEventListener("click", function(event) {
+    const targetClass = event.target.classList.value;
+    console.log(targetClass);
     clearList();
-    console.log(444)
-    if (targetClass.includes("circle__black")) {
+    if (event.target.closest(".circle__black")) {
       slides[0].classList.add(activeClass);
-    } else if (targetClass.includes("circle__gray")) {
+    } else if (event.target.closest(".circle__gray")) {
       slides[1].classList.add(activeClass);
-    } else if (targetClass.includes("circle__green")) {
+    } else if (event.target.closest(".circle__green")) {
       slides[2].classList.add(activeClass);
-    } else if (targetClass.includes("circle__blue")) {
+    } else if (event.target.closest(".circle__blue")) {
       slides[3].classList.add(activeClass);
-    } else if (targetClass.includes("circle__red")) {
+    } else if (event.target.closest(".circle__red")) {
       console.log("Клик!");
       slides[4].classList.add(activeClass);
     }
   });
-
 })();
