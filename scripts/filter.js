0e15eb26a670b132ea34d4afdcbf0fed5bc827f8
daATva <riven1231@mail.ru>
2023-12-09 00:58:43 +0400

    review slider

--- scripts/filter.js
+++ scripts/filter.js
@@ -1,9 +1,6 @@
 (function() {
   const elem = document.querySelector(".products__list");
-  const iso = new Isotope(elem, {
-    itemSelector: ".products__item",
-    filter: '.popular'
-  });
+
 
   const controlls = document.querySelectorAll(".filter__link");
   const activeClass = "filter__item--active";
@@ -20,9 +17,6 @@
 
       control.closest(".filter__item").classList.add(activeClass);
 
-      iso.arrange({
-        filter: `.${filterName}`
-      })
     });
   });
 })();
--- scripts/shirt-slider.js
+++ scripts/shirt-slider.js
@@ -5,82 +5,31 @@
 
   const lessons = document.querySelector(".circle");
 
-  lessons.addEventListener("click", function (event) {
-    if (event.target.closest(".circle__black")) {
-      slides[1].classList.remove(activeClass);
-      slides[2].classList.remove(activeClass);
-      slides[3].classList.remove(activeClass);
-      slides[4].classList.remove(activeClass);
+
+  function clearList(){
+    slides[1].classList.remove(activeClass);
+    slides[2].classList.remove(activeClass);
+    slides[3].classList.remove(activeClass);
+    slides[4].classList.remove(activeClass);
+    slides[0].classList.remove(activeClass);
+  }
+
+
+  lessons.addEventListener("click", function(event) {
+    clearList();
+    console.log(444)
+    if (targetClass.includes("circle__black")) {
       slides[0].classList.add(activeClass);
-    }
-  });
-
-  lessons.addEventListener("click", function (event) {
-    if (event.target.closest(".circle__gray")) {
-      slides[0].classList.remove(activeClass);
-      slides[2].classList.remove(activeClass);
-      slides[3].classList.remove(activeClass);
-      slides[4].classList.remove(activeClass);
+    } else if (targetClass.includes("circle__gray")) {
       slides[1].classList.add(activeClass);
-    }
-  });
-
-  lessons.addEventListener("click", function (event) {
-    if (event.target.closest(".circle__green")) {
-      slides[0].classList.remove(activeClass);
-      slides[1].classList.remove(activeClass);
-      slides[3].classList.remove(activeClass);
-      slides[4].classList.remove(activeClass);
+    } else if (targetClass.includes("circle__green")) {
       slides[2].classList.add(activeClass);
-    }
-  });
-
-  lessons.addEventListener("click", function (event) {
-    if (event.target.closest(".circle__blue")) {
-      slides[0].classList.remove(activeClass);
-      slides[1].classList.remove(activeClass);
-      slides[2].classList.remove(activeClass);
-      slides[4].classList.remove(activeClass);
+    } else if (targetClass.includes("circle__blue")) {
       slides[3].classList.add(activeClass);
-    }
-  });
-
-  lessons.addEventListener("click", function (event) {
-    if (event.target.closest(".circle__red")) {
+    } else if (targetClass.includes("circle__red")) {
       console.log("Клик!");
-      slides[0].classList.remove(activeClass);
-      slides[1].classList.remove(activeClass);
-      slides[2].classList.remove(activeClass);
-      slides[3].classList.remove(activeClass);
       slides[4].classList.add(activeClass);
     }
   });
 
-  // const handleClick = () => {
-  //   console.log('Click')
-  //   slides[index].classList.remove(activeClass);
-  //   slides[0].classList.add(activeClass);
-  // }
-
-  // circles.forEach(button =>{
-  //   button.addEventListener('click' , handleClick)
-  // })
-
-  // function showConsole() {
-  //   console.log("Ура!");
-  // }
-
-  //   lessons.addEventListener("click" , function (event){
-  //     if(event.target.closest('.circle__black')) {
-  //       slides[index].classList.remove(activeClass);
-  //       slides[0].classList.add(activeClass);
-  //     }
-  // });
-
-  //   lessons.addEventListener("click" , function (event){
-  //     if(event.target.closest('.circle__gray')) {
-  //       slides[index].classList.remove(activeClass);
-  //       slides[1].classList.add(activeClass);
-  //     }
-  //   });
 })();
